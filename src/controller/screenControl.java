5cb0af27048aec2dc6e4212ea31d0eaf5e9d47a1
Kuba <j.swierlik@o2.pl>
2019-05-07 15:28:10 +0200

    model->logics

--- src/controller/screenControl.java
+++ src/controller/screenControl.java
@@ -9,11 +9,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import model.gameThreadClass;
 
-import static model.model.TORREADOR_EDGE;
-import static model.model.actual_bull_pos;
-import static model.model.getLevel;
+import static model.logics.TORREADOR_EDGE;
+import static model.logics.actual_bull_pos;
+import static model.logics.getLevel;
 import view.drawingGraphics;
-import static model.model.BULL_POS_X;
+import static model.logics.BULL_POS_X;
 
 /**
  *
@@ -35,9 +35,9 @@ public class screenControl extends view.gui implements ActionListener {
     public void actionPerformed(ActionEvent event){
      
         getGameWindow().remove(mainView);
-        actual_bull_pos=BULL_POS_X-i*model.model.bullSpeed;        
+        actual_bull_pos=BULL_POS_X-i*model.logics.bullSpeed;        
         mainView=new drawingGraphics(i);
-        model.model.mainViewCopy=mainView;
+        model.logics.mainViewCopy=mainView;
                 
         getGameWindow().getContentPane().add(mainView); 
         getGameWindow().getContentPane().revalidate();
@@ -48,9 +48,9 @@ public class screenControl extends view.gui implements ActionListener {
             gameThreadClass.timer.stop();
         }                     
         //if you were too late stop drawing and activate lost phase
-        else if(actual_bull_pos<=TORREADOR_EDGE && model.model.getBullRunFurther()==0){
+        else if(actual_bull_pos<=TORREADOR_EDGE && model.logics.getBullRunFurther()==0){
             gameThreadClass.timer.stop();
-            model.model.lost();                               
+            model.logics.lost();                               
         }              
     }       
 }
